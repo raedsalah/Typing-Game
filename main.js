4ff710599a90d29b329eb96e2702eadb1b7dba74
Raed Salah <dkraed@gmail.com>
2018-10-29 17:07:57 +0800

    word match checking

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let score = 0;
 let isPlaying;
 
 //dom variables
-const wordIput = document.querySelector('#word-input');
+const wordInput = document.querySelector('#word-input');
 const currentWord = document.querySelector('#current-word');
 const scoreDisplay = document.querySelector('#score');
 const timeDisplay = document.querySelector('#time');
@@ -40,8 +40,28 @@ const words = [
 function init(){
     // Load random word from array
     showWord(words);
+    // Check word input
+    wordInput.addEventListener('input', startMatch);
     //call countdown for every sec
     setInterval(countdown, 1000);
+    //check game status
+    setInterval(checkStatus, 50);
+}
+//check word match
+function startMatch(){
+    if(matchWord()){
+        console.log('MATCH!!!');
+    }
+}
+// matc to input
+function matchWord(){
+    if(wordInput.value === currentWord.innerHTML){
+        message.innerHTML = 'correct!!';
+        return true;
+    } else {
+        message.innerHTML = '';
+        return false;
+    }
 }
 
 //Pick and show random word
@@ -50,4 +70,24 @@ function showWord(words){
     const randIndex = Math.floor(Math.random() * words.length);
     // output a random word
     currentWord.innerHTML = words[randIndex];
+}
+
+//Countdown timer
+function countdown(){
+    // Check time is still running
+    if(time > 0){
+        // decrement time every sec
+        time--;
+    } else if (time === 0) {
+        isPlaying = false;
+    }
+    //display time
+    timeDisplay.innerHTML = time;
+}
+
+//check game status
+function checkStatus(){
+    if(!isPlaying && time === 0){
+        message.innerHTML = 'Game Over!!!';
+    }
 }
