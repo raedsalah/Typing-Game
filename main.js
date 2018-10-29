f12637826e39b979cde8b475c84088a43c05c5f6
Raed Salah <dkraed@gmail.com>
2018-10-29 16:57:24 +0800

    game intiated, words array set and display random words on refresh fromm array

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+window.addEventListener('load', init);
+
 //global variables 
 let time = 5;
 let score = 0;
@@ -11,7 +13,7 @@ const timeDisplay = document.querySelector('#time');
 const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
 
-const words =[
+const words = [
     'raed',
     'tariq',
     'salah',
@@ -32,4 +34,20 @@ const words =[
     'and',
     'breaking',
     'keyboards'
-]
+];
+
+//Initializing game
+function init(){
+    // Load random word from array
+    showWord(words);
+    //call countdown for every sec
+    setInterval(countdown, 1000);
+}
+
+//Pick and show random word
+function showWord(words){
+    //Generate random array index
+    const randIndex = Math.floor(Math.random() * words.length);
+    // output a random word
+    currentWord.innerHTML = words[randIndex];
+}
