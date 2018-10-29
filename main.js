382631bc05aa238a432644a73d1e329530d555d0
Raed Salah <dkraed@gmail.com>
2018-10-29 16:43:59 +0800

    variables decalred and array of words created

---
+++ main.js
@@ -0,0 +1,35 @@
+//global variables 
+let time = 5;
+let score = 0;
+let isPlaying;
+
+//dom variables
+const wordIput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+
+const words =[
+    'raed',
+    'tariq',
+    'salah',
+    'dog',
+    'hat',
+    'car',
+    'hero',
+    'java',
+    'this',
+    'is',
+    'a',
+    'test',
+    'of ',
+    'random',
+    'words',
+    'investigating',
+    'situations',
+    'and',
+    'breaking',
+    'keyboards'
+]
